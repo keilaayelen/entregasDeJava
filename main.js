2c1c6071c0fc232b5a6a1ee32bfb46daf7b7ee27
keila ayelen lima <keila_alima@hotmail.com>
2023-02-12 22:57:18 -0300

    ediciones nocturnas

--- main.js
+++ main.js
@@ -40,15 +40,75 @@ while (fuera) {
 }
 
 
+class Gatitos {
+    constructor(sexo, color, edad) {
+        this.sexo = sexo;
+        this.color = color;
+        this.edad = edad;
+    }
+}
+
+let sexo = [
+    { sexo: "Hembra", color: "claro" },
+    { sexo: "Macho", color: "oscuro" },
+]
+
+function adoptar() {
+    let gatos = prompt("Ingrese el sexo que desea adoptar");
+    let colores = prompt("Ingrese si quiere claro u oscuro su michi");
+    let edades = prompt("Ingrese si quiere un gatito bebe o grande");
+    const nuevaAdopcion = new Gatitos(gatos, colores, edades)
+    arrayCarrito.push(nuevaAdopcion)
+    alert("Adopcion en curso")
+}
+
+let arrayCarrito = []
 
 
+//Llamado a function
+adoptar();
+
+function verAdopcion() {
+    arrayCarrito.forEach((gatito) => {
+        alert(`Usted eligio ${gatito.sexo} y de un colorcito ${gatito.color} con una edad de ${gatito.edad}`)
+    })
+}
+
+//Llamado a function
+verAdopcion();
+
+// Elegir a quien adoptar
+class ElegirHembra {
+    constructor(lupa, tana, micha, flora, juana) {
+        this.lupa = lupa;
+        this.tana = tana;
+        this.micha = micha;
+        this.flora = flora;
+        this.juana = juana;
+    }
+}
 
 
+let hembras = [
+    { hembras: "Lupa", color: "claro" },
+    { hembras: "Tana", color: "oscuro" },
+    { hembras: "Micha", color: "claro" },
+    { hembras: "Flora", color: "oscuro" },
+    { hembras: "Juana", color: "claro" },
+]
 
 
+function elegirGato() {
+    let hembras = prompt("Quedan solo hembras, ponga que si para poder continuar");
+    let color = prompt("Ingrese si quiere claro u oscuro su pelaje");
+    const nuevaEleccion = new Eleccion(hembras, color)
+    arrayCarrito2.push(nuevaEleccion)
+    alert("Adopcion en curso")
+}
 
+let arrayCarrito2 = []
 
-
+elegirGato();
 
 
 function despedir() {
@@ -58,3 +118,12 @@ function despedir() {
 
 //Llamado a function
 despedir();
+
+
+
+
+
+
+
+
+/* Ingrese si quiere adoptar a \n 1: Lupa \n 2: Tana \n 3: Micha \n 4: Flora \n 5: Juana  */
